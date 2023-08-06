2a84535eb41049c7051c0c35ddf9b9a284e51617
yuri <79948608+yurimanickam@users.noreply.github.com>
2023-08-07 09:50:59 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -6,7 +6,7 @@ let beamOffset = 40;
 let beamWidth = 15;
 
 function setup_wallpaper(pWallpaper) {
-pWallpaper.output_mode(DEVELOP_GLYPH  ); //GRID_WALLPAPER// //DEVELOP_GLYPH\\
+pWallpaper.output_mode(GRID_WALLPAPER  ); //GRID_WALLPAPER// //DEVELOP_GLYPH\\
   pWallpaper.resolution(NINE_LANDSCAPE);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
@@ -28,9 +28,13 @@ beamBottom();
 platform();
 
 beamTop();
-carBody();
-wheelMethod();
-CarTwo();
+if (random() < 0.6) {
+  push(); // Save the current transformation state // Move the car body to the tile position // Randomly rotate the car body
+  carBody();
+  pop(); // Restore the original transformation state
+}
+
+
 }
 
 
@@ -124,87 +128,95 @@ function beamBottom(){
 }
 
 function carBody(){
-  let scalingFactor = 1.20;
-  let shapeX = 100; // Initial X position of the shape
-  let shapeY = 100; // Initial Y position of the shape
-  Push(); // Save the current transformation state
+  let scalingFactor = 1.50;
+  let shapeX = 40; // Initial X position of the shape
+  let shapeY = 110; // Initial Y position of the shape
+  push(); // Save the current transformation state
    // Save the current transformation state
-  translate(shapeX, shapeY); // Move the shape based on shapeX and shapeY
+  translate(shapeX, shapeY);
+  scale(1.5,1.5) // Move the shape based on shapeX and shapeY
   fill("#D67070");
   stroke("#D67070");
   beginShape();
-  curveVertex(106.06 * scalingFactor, 18.28 * scalingFactor);
-  curveVertex(106.06 * scalingFactor, 18.28 * scalingFactor);
-  curveVertex(88.66 * scalingFactor, 11.81 * scalingFactor);
-  curveVertex(85.63 * scalingFactor, 10.68 * scalingFactor);
-  curveVertex(78.82 * scalingFactor, 10.11 * scalingFactor);
-  curveVertex(73.74 * scalingFactor, 10.11 * scalingFactor);
-  curveVertex(71.78 * scalingFactor, 8.29 * scalingFactor);
-  curveVertex(70.07 * scalingFactor, 6.39 * scalingFactor);
-  curveVertex(68.19 * scalingFactor, 6.39 * scalingFactor);
-  curveVertex(66.2 * scalingFactor, 6.39 * scalingFactor);
-  curveVertex(64.42 * scalingFactor, 6.4 * scalingFactor);
-  curveVertex(62.7 * scalingFactor, 6.5 * scalingFactor);
-  curveVertex(61.36 * scalingFactor, 5.68 * scalingFactor);
-  curveVertex(61.09 * scalingFactor, 3.98 * scalingFactor);
-  curveVertex(59.13 * scalingFactor, 2.1 * scalingFactor);
-  curveVertex(57.14 * scalingFactor, 2.1 * scalingFactor);
-  curveVertex(54.87 * scalingFactor, 2.1 * scalingFactor);
-  curveVertex(52.9 * scalingFactor, 2.1 * scalingFactor);
-  curveVertex(49.26 * scalingFactor, 2.18 * scalingFactor);
-  curveVertex(27.61 * scalingFactor, 2.26 * scalingFactor);
-  curveVertex(27.57 * scalingFactor, 0.26 * scalingFactor);
-  curveVertex(27.57 * scalingFactor, 0 * scalingFactor);
-  curveVertex(25.46 * scalingFactor, 0 * scalingFactor);
-  curveVertex(0, 9.34 * scalingFactor);
-  curveVertex(0, 20.31 * scalingFactor);
-  curveVertex(4.39 * scalingFactor, 20.31 * scalingFactor);
-  curveVertex(4.39 * scalingFactor, 26.73 * scalingFactor);
-  curveVertex(106.68 * scalingFactor, 26.73 * scalingFactor);
-  curveVertex(120.39 * scalingFactor, 24.6 * scalingFactor);
-  curveVertex(105.08 * scalingFactor, 18.28 * scalingFactor);
-  curveVertex(106.06 * scalingFactor, 18.28 * scalingFactor);
+  vertex(112.86, 28.37);
+  vertex(89.57, 17.56);
+  bezierVertex(87.01, 16.48, 84.34, 15.94, 81.66, 15.94);
+  vertex(68.44, 15.94);
+  vertex(66.35, 13.86);
+  bezierVertex(64.2, 11.71, 61.6, 10.56, 58.93, 10.56);
+  vertex(49.83, 10.56);
+  vertex(49.83, 0);
+  vertex(40.04, 0);
+  bezierVertex(33.16, 0, 26.35, 1.99, 20.08, 5.84);
+  vertex(0, 18.18);
+  vertex(0, 30.69);
+  vertex(4.67, 30.69);
+  vertex(4.67, 38.01);
+  vertex(113.52, 38.01);
+  vertex(128.11, 35.58);
+  vertex(112.86, 28.37);
 
   endShape(CLOSE);
   pop();
 }
 
-function CarTwo(){
-push(); // Save the current transformation state
-   // Save the current transformation state
-  translate(100, 1); // Move the shape based on shapeX and shapeY
-  fill("#D67070");
-  stroke("#D67070");
-  scale(1,1);
-stroke('rgba(0,0,0,0)')
-strokeCap(PROJECT);
-strokeJoin(MITER);
-fill("#ca291c")
-globalAlpha = 0.49;
-beginShape();
-vertex(112.86,28.37);
-vertex(89.57,17.560000000000002);
-bezierVertex(87.00999999999999,16.480000000000004,84.33999999999999,15.940000000000001,81.66,15.940000000000001);
-vertex(68.44,15.940000000000001);
-vertex(66.35,13.860000000000001);
-bezierVertex(64.19999999999999,11.71,61.599999999999994,10.560000000000002,58.92999999999999,10.560000000000002);
-vertex(49.82999999999999,10.560000000000002);
-vertex(49.82999999999999,0);
-vertex(40.03999999999999,0);
-bezierVertex(33.15999999999999,0,26.349999999999994,1.99,20.07999999999999,5.84);
-vertex(0,18.18);
-vertex(0,30.689999999999998);
-vertex(4.67,30.689999999999998);
-vertex(4.67,38.01);
-vertex(113.52,38.01);
-vertex(128.10999999999999,35.58);
-vertex(112.85999999999999,28.369999999999997);
-endShape();
-
+function cd() {
+  push(); // Save the current transformation state
+  translate(0, 0); // Move the shape based on shapeX and shapeY
+  fill(202, 41, 28, 125); // Set the fill color with transparency (125 is the alpha value)
+  noStroke(); // Disable stroke for this shape
+  beginShape();
+  vertex(112.86, 28.37);
+  vertex(89.57, 17.56);
+  bezierVertex(87.01, 16.48, 84.34, 15.94, 81.66, 15.94);
+  vertex(68.44, 15.94);
+  vertex(66.35, 13.86);
+  bezierVertex(64.2, 11.71, 61.6, 10.56, 58.93, 10.56);
+  vertex(49.83, 10.56);
+  vertex(49.83, 0);
+  vertex(40.04, 0);
+  bezierVertex(33.16, 0, 26.35, 1.99, 20.08, 5.84);
+  vertex(0, 18.18);
+  vertex(0, 30.69);
+  vertex(4.67, 30.69);
+  vertex(4.67, 38.01);
+  vertex(113.52, 38.01);
+  vertex(128.11, 35.58);
+  vertex(112.86, 28.37);
+  endShape(CLOSE);
   pop();
 }
 
 
+function test_WHY(){
+  let shapeX = 100; // Initial X position of the shape
+  let shapeY = 100; // Initial Y position of the shape
+  push(); // Save the current transformation state
+   // Save the current transformation state
+  translate(shapeX, shapeY); // Move the shape based on shapeX and shapeY
+  fill("#0f0d0d");
+  stroke("#141212");
+  beginShape();
+  vertex(112.86, 28.37);
+  vertex(89.57, 17.56);
+  bezierVertex(87.01, 16.48, 84.34, 15.94, 81.66, 15.94);
+  vertex(68.44, 15.94);
+  vertex(66.35, 13.86);
+  bezierVertex(64.2, 11.71, 61.6, 10.56, 58.93, 10.56);
+  vertex(49.83, 10.56);
+  vertex(49.83, 0);
+  vertex(40.04, 0);
+  bezierVertex(33.16, 0, 26.35, 1.99, 20.08, 5.84);
+  vertex(0, 18.18);
+  vertex(0, 30.69);
+  vertex(4.67, 30.69);
+  vertex(4.67, 38.01);
+  vertex(113.52, 38.01);
+  vertex(128.11, 35.58);
+  vertex(112.86, 28.37);
 
+  endShape(CLOSE);
+  pop();
+}
 
 
