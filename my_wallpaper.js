703955f999575f76e538a2e258336b423a0752ac
yuri <79948608+yurimanickam@users.noreply.github.com>
2023-08-07 21:17:38 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -2,8 +2,9 @@
 let x = 100;//global x and y 
 //structure variables, 40 on beamOffset makes the beams line up.
 
-let randomHeight = false
-let platformHeight = 40;//adjust the height the platform spawns at.
+let randomHeight = false // make the heights of the platforms have variability
+let heightRandomness = 1; //variability of tha random height
+let platformHeight = 60;//adjust the height the platform spawns at.
 let platformThickness = 20;
 let platformDepth = 15;//how tall the ellipse is in relation to its width, adjusting depth
 
@@ -12,10 +13,10 @@ let beamWidth = 12;
 
 
 
-
+let cloud = [];
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.output_mode(DEVELOP_GLYPH);
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
@@ -31,17 +32,16 @@ function wallpaper_background() {
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   if (randomHeight == true) {
-    randHi = pHeight + random(-10, 10);
+    randHi = pHeight + random(-10 * heightRandomness, 10 * heightRandomness);
   } else {
     randHi = pHeight;
   }
-
   beamBottom(randHi);
   platform(randHi);
   beamTop(randHi);
   //if (random() < 0.6) {
     //push(); 
-    test_WHY();
+    carBody(randHi);
     //pop(); // Restore the original transformation state
   //}
   
@@ -50,6 +50,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 
 
 //precursor statement for easier input interaction
+
 let pHeight = 200 - platformHeight;
 function beamBottom(pHeight){
   noStroke();
@@ -104,32 +105,33 @@ function beamTop(pHeight) {
 }
 }
 
-function test_WHY(){
+function carBody(pHeight){
 push();
-  translate(100, 100); // Move the shape based on shapeX and shapeY
+  translate(20, -41 + pHeight);
+  scale(1.2,1,2);
+  //translate the elements all at once, or individually?
   fill("#0f0d0d");
-  stroke('rgba(0,0,0,0)')
-  strokeCap(PROJECT);
-  strokeJoin(MITER);
   fill("#ca291c")
   beginShape();
   vertex(112.86,28.37);
-  vertex(89.57,17.560000000000002);
-  bezierVertex(87.00999999999999,16.480000000000004,84.33999999999999,15.940000000000001,81.66,15.940000000000001);
-  vertex(68.44,15.940000000000001);
-  vertex(66.35,13.860000000000001);
-  bezierVertex(64.19999999999999,11.71,61.599999999999994,10.560000000000002,58.92999999999999,10.560000000000002);
-  vertex(49.82999999999999,10.560000000000002);
-  vertex(49.82999999999999,0);
-  vertex(40.03999999999999,0);
-  bezierVertex(33.15999999999999,0,26.349999999999994,1.99,20.07999999999999,5.84);
+  vertex(89.57,17.56);
+  bezierVertex(87.01,16.48,84.34,15.94,81.66,15.94);
+  vertex(68.44,15.94);
+  vertex(66.35,13.86);
+  bezierVertex(64.2,11.71,61.6,10.56,58.93,10.56);
+  vertex(49.83,10.56);
+  vertex(49.83,0);
+  vertex(40.034,0);
+  bezierVertex(33.16,0,26.35,1.99,20.08,5.84);
   vertex(0,18.18);
-  vertex(0,30.689999999999998);
-  vertex(4.67,30.689999999999998);
+  vertex(0,30.69);
+  vertex(4.67,30.69);
   vertex(4.67,38.01);
   vertex(113.52,38.01);
-  vertex(128.10999999999999,35.58);
-  vertex(112.85999999999999,28.369999999999997);
+  vertex(128.11,35.58);
+  vertex(112.86,28.37);
   endShape();
 pop();
 }
+
+
